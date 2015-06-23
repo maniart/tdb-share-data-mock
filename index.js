50c7b395300d60e1e1a117720b16fae379969964
maniart <mani.art@gmail.com>
2015-06-23 14:33:21 -0400

    NaNaNaNaNaNaNaNaaaaa

--- index.js
+++ index.js
@@ -13,8 +13,26 @@ app.use(function(req, res, next) {
 });
 
 app.get('*', function(req, res) {
-	var record = random(0, data.length);
-	res.send(data[record]);
+	var id = random(0, data.length),
+		record = data[id],
+		email_expires = record.estimated_expires;
+	
+	record.twitter_key = record.twitter_key || 0;
+	record.facebook_key = record.facebook_key || 0;
+	record.email_key = record.estimated_key || 0;
+	record.email_expires = email_expires;
+	
+	delete record.estimated_key;
+	delete record.estimated_expires;
+
+	var total = record.total = record['twitter_key'] + record['facebook_key'] + record['email_key'];
+
+	record.facebook_percentage = record.facebook_key / total * 100 || 0;
+	record.twitter_percentage = record.twitter_key / total * 100 || 0;
+	record.email_percentage = record.email_key / total * 100 || 0;
+	//console.log('-----------------------------------------------------------------------------------------------------------------------------------------------\n', record);
+	res.send(record);
+
 });
 
 app.listen(3000);
